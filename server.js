fdd0b6586ec85c07310579f385c70e40066c4fa9
Ajinkya <ajinkyamohod3@gmail.com>
2017-08-16 00:13:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req, res) {
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/Bio', function(req, res) {
+     res.sendFile(path.join(__dirname, 'ui', '.html'));
 });
 
 app.get('/article-two', function(req, res) {
