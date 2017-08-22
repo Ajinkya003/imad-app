17c64fd4a7f23c323b760cd689407aed1e6c5215
Ajinkya <ajinkyamohod3@gmail.com>
2017-08-22 19:09:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,8 +117,8 @@ app.get('/article-three', function(req, res) {
     res.send('Article-three will be applied here');
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
